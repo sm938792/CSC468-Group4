ce17b63a6629f0073b20a073f726db09becc1182
sm938792 <70286176+sm938792@users.noreply.github.com>
2022-04-17 17:59:10 -0400

    init git repo

---
+++ discordbot/index.js
@@ -0,0 +1,27 @@
+// Puts discord.js package info into a variable
+const Discord = require("discord.js")
+
+// .env variables into a global variable with process.env
+require("dotenv").config()
+
+// Creates bot client to access Discord API
+const client = new Discord.Client({
+    intents: [
+        "GUILDS", // Guilds are Discord Servers
+        "GUILD_MESSAGES" // Things to look out for
+    ]
+})
+
+// Ready Event Listener - triggers when bot login is successful
+client.on("ready", () => {
+    console.log(`${client.user.tag} login successful`)
+})
+
+client.on("messageCreate", (message) => {
+    if(message.content == "Hi!") {
+        message.reply("Hello World!")
+    }
+})
+
+// Bot login
+client.login(process.env.TOKEN)
